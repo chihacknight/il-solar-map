286c85d61c3926fcad83c97b4c0f04c99dd7ace9
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-05-16 20:50:32 -0500

    run through all of the file

--- aggregate-data-by-county.py
+++ aggregate-data-by-county.py
@@ -7,12 +7,14 @@ ac_kwh_index = 8
 with open('data/report-3-census-tract-rev.csv', "r") as csvfile:
     datareader = csv.reader(csvfile)
     for row in datareader:
-        print(row)
-        
+        # print(row)
+        if(row[county_index]=="County"):
+            continue
+
         if row[county_index] in data:
             data[row[county_index]][0] += 1
-            data[row[county_index]][1] += int(row[ac_kwh_index])
+            data[row[county_index]][1] += float(row[ac_kwh_index])
         else:
-            data[row[county_index]] = [1, int(row[ac_kwh_index])]
+            data[row[county_index]] = [1, float(row[ac_kwh_index])]
 
 print(data)
