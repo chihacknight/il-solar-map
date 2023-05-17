f47f5f732043d821f1fb8ffe0a8688507b48e0db
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-05-16 20:57:42 -0500

    clean up name of county

--- aggregate-data-by-county.py
+++ aggregate-data-by-county.py
@@ -7,14 +7,18 @@ ac_kwh_index = 8
 with open('data/report-3-census-tract-rev.csv', "r") as csvfile:
     datareader = csv.reader(csvfile)
     for row in datareader:
-        
-        if(row[county_index]=="County"):
+
+        raw_county_name = row[county_index]
+
+        county_name = raw_county_name.upper().replace(" COUNTY", "")
+
+        if(county_name=="COUNTY"):
             continue
 
-        if row[county_index] in data:
-            data[row[county_index]][0] += 1
-            data[row[county_index]][1] += float(row[ac_kwh_index])
+        if county_name in data:
+            data[county_name][0] += 1
+            data[county_name][1] += float(row[ac_kwh_index])
         else:
-            data[row[county_index]] = [1, float(row[ac_kwh_index])]
+            data[county_name] = [1, float(row[ac_kwh_index])]
 
 print(data)
