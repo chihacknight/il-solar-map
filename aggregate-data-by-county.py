429d343ec466577da26a06c6d777f0b50ca8c78d
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-05-16 20:48:19 -0500

    open and loop through csv

---
+++ aggregate-data-by-county.py
@@ -0,0 +1,18 @@
+import csv
+
+data = {}
+county_index = 15
+ac_kwh_index = 8
+
+with open('data/report-3-census-tract-rev.csv', "r") as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        print(row)
+        
+        if row[county_index] in data:
+            data[row[county_index]][0] += 1
+            data[row[county_index]][1] += int(row[ac_kwh_index])
+        else:
+            data[row[county_index]] = [1, int(row[ac_kwh_index])]
+
+print(data)
