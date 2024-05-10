43accbee3a196cb5dc4d146ec9e00b87485fd70b
Derek Eder <derek.eder@gmail.com>
2024-05-10 17:06:31 -0500

    added planned projects to reference tables

--- data/scripts/run_all.sh
+++ data/scripts/run_all.sh
@@ -31,16 +31,25 @@ python get_tract_districts.py
 python aggregate_data.py
 
 # format the csv files
-csvcut ../final/solar-projects-by-county.csv -c "county_name,total_kw,utility_kw,cs_kw,dg_large_kw,dg_small_kw" > ../final/solar-projects-by-county-formatted.csv
+csvcut ../final/solar-projects-by-county.csv -c "county_name,total_kw,utility_kw,cs_kw,dg_large_kw,dg_small_kw,planned_total_kw,planned_utility_kw,planned_cs_kw,planned_dg_large_kw,planned_dg_small_kw" > ../final/solar-projects-by-county-formatted.csv
+sed 1d ../final/solar-projects-by-county-formatted.csv > tmp.txt
+sed '1 s/^/county,total,utility,cs,dg_large,dg_small,pl_total,pl_utility,pl_cs,pl_dg_large,pl_dg_small\n/' tmp.txt > ../final/solar-projects-by-county-formatted.csv
 
-csvcut ../final/solar-projects-by-place.csv -c "place,total_kw,utility_kw,cs_kw,dg_large_kw,dg_small_kw" > ../final/solar-projects-by-place-formatted.csv
+csvcut ../final/solar-projects-by-place.csv -c "place,total_kw,utility_kw,cs_kw,dg_large_kw,dg_small_kw,planned_total_kw,planned_utility_kw,planned_cs_kw,planned_dg_large_kw,planned_dg_small_kw" > ../final/solar-projects-by-place-formatted.csv
+sed 1d ../final/solar-projects-by-place-formatted.csv > tmp.txt
+sed '1 s/^/place,total,utility,cs,dg_large,dg_small,pl_total,pl_utility,pl_cs,pl_dg_large,pl_dg_small\n/' tmp.txt > ../final/solar-projects-by-place-formatted.csv
 
-csvcut ../final/solar-projects-by-il-house.csv -c "house_district,legislator,party,total_kw,utility_kw,cs_kw,dg_large_kw,dg_small_kw" > ../final/solar-projects-by-il-house-formatted.csv
+csvcut ../final/solar-projects-by-il-house.csv -c "house_district,legislator,party,total_kw,utility_kw,cs_kw,dg_large_kw,dg_small_kw,planned_total_kw,planned_utility_kw,planned_cs_kw,planned_dg_large_kw,planned_dg_small_kw" > ../final/solar-projects-by-il-house-formatted.csv
+sed 1d ../final/solar-projects-by-il-house-formatted.csv > tmp.txt
+sed '1 s/^/house_district,legislator,party,total,utility,cs,dg_large,dg_small,pl_total,pl_utility,pl_cs,pl_dg_large,pl_dg_small\n/' tmp.txt > ../final/solar-projects-by-il-house-formatted.csv
 
-csvcut ../final/solar-projects-by-il-senate.csv -c "senate_district,legislator,party,total_kw,utility_kw,cs_kw,dg_large_kw,dg_small_kw" > ../final/solar-projects-by-il-senate-formatted.csv
+csvcut ../final/solar-projects-by-il-senate.csv -c "senate_district,legislator,party,total_kw,utility_kw,cs_kw,dg_large_kw,dg_small_kw,planned_total_kw,planned_utility_kw,planned_cs_kw,planned_dg_large_kw,planned_dg_small_kw" > ../final/solar-projects-by-il-senate-formatted.csv
+sed 1d ../final/solar-projects-by-il-senate-formatted.csv > tmp.txt
+sed '1 s/^/senate_district,legislator,party,total,utility,cs,dg_large,dg_small,pl_total,pl_utility,pl_cs,pl_dg_large,pl_dg_small\n/' tmp.txt > ../final/solar-projects-by-il-senate-formatted.csv
 
 # cleanup
 echo "cleanup temp data"
+rm tmp.txt
 rm ../raw/eia860_raw.csv
 rm ../raw/eia860.csv
 rm ../raw/ilabp.csv
